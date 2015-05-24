04ecf72d05caeed54d1f6102c4cd80cff2654099
Henri Knochenhauer <hknochi@gmail.com>
2015-05-24 22:36:28 +0200

    remove tests (absolutely unuseful)

--- src/main/webapp/WEB-INF/views/includes/footer.jsp
+++
@@ -1,6 +0,0 @@
-<!-- Footer -->
-<div id="footer">
-      <div class="container">
-        <p class="text-muted">Place sticky footer content here.</p>
-      </div>
-    </div>
--- src/main/webapp/resources/js/diagramms.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	console.log(jspArgs.get('requestTypes'));
-	Morris.Donut({
-		element : 'morris-chart-requestTypes',
-		data : jspArgs.get('requestTypes')
-	});
-});
